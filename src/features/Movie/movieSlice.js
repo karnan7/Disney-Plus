e3a011cdcdbad443b582134184a0e1348c974ea1
karnan7 <adithkayalad@gmail.com>
2023-03-26 18:53:47 +0530

    first commit

---
+++ src/features/Movie/movieSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState ={
+    movie:""
+}
+
+const movieSlice = createSlice({
+    movie:"",
+    initialState,
+    reducers:{
+        
+    }
+})
