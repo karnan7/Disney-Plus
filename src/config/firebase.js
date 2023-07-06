f777b24eae688d958b173796bff6757b64618945
karnan7 <adithkayalad@gmail.com>
2023-07-06 23:39:14 +0530

    gitignored

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import "firebase/compat/auth";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCWh51WqzOx_EcryxpnXUv2EsT-Fw7Tszo",
-    authDomain: "disneyplus-e86d7.firebaseapp.com",
-    projectId: "disneyplus-e86d7",
-    storageBucket: "disneyplus-e86d7.appspot.com",
-    messagingSenderId: "289891573875",
-    appId: "1:289891573875:web:efc9930da8ee8cd1428a06",
-    measurementId: "G-Y5LPHWS1PX"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
